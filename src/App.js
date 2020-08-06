56a8c5be9d94dbecb3e8e31f9cd2f7f5e2eb0049
tranmanhhungdn1201 <tranmanh.hungdn1201@gmail.com>
2020-08-06 22:06:02 +0700

    setup api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { BrowserRouter, Redirect, Route, Switch, Link } from 'react-router-dom';
 import NotFound from './components/NotFound';
 import './App.css';
 import Header from './components/Header';
+import productApi from 'api/productApi';
 
 // Lazy load - Code splitting
 const Photo = React.lazy(() => import('./features/Photo'));
 
 function App() {
+  const [ productList, setProductList ] = useState([]);
+  useEffect(() => {
+    const fetchProductList = async () => {
+      try {
+        const params = {
+           _page: 1,
+           _limit: 10
+        }
+        const response = await productApi.getAll(params);
+        console.log(response);
+      } catch (error) {
+        console.log('Fail to fetch product list: ', error);
+      }
+    }
+
+    fetchProductList();
+  }, []);
+
   return (
     <div className="App">
       <Suspense fallback={<div>Loading ...</div>}>
